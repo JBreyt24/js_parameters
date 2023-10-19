b9bea386a6c0a86c7a485b63ef4d7715e8f4033a
Josh Breytspraak <133163031+JBreyt24@users.noreply.github.com>
2023-10-19 00:51:26 -0500

    Add files via upload

---
+++ Breytspraak_Josh_Parameters/Breytspraak_Josh_Parameters.js
@@ -0,0 +1,25 @@
+// function greetSomeone(person) {
+//     if (person == "Martin") {
+//         console.log("Hello, partner!");
+//     }
+//     else {
+//         console.log("Greetings, Earthling!");
+//     }
+// }
+// greetSomeone("Martin");
+// greetSomeone("Jerry");
+
+//used above code as a template to get started
+
+
+
+function greet(name, time) {
+    if (name == "Count Dooku") {
+        console.log("I'm coming for you, Dooku!");
+    } else {
+        console.log("Good " + time + " " + name + "!");
+
+    }
+}
+greet("Anakin", "morning");
+greet("Count Dooku", "day");
